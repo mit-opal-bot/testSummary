9021db09c9a90b768c911e2e3324b2dad6275964
Justin Anderson <jander@mit.edu>
2018-04-03 08:18:31 -0400

    Elimination

--- src/edu/mit/jenkins/Utils.groovy
+++ src/edu/mit/jenkins/Utils.groovy
@@ -43,37 +43,37 @@ def warningsInfo() {
     def warningActions = currentBuild.rawBuild.getActions(WarningsResultAction.class)
     result = warningActions[0].getResult()
     println result
-    println result.getNumberOfNewWarnings()
-    println result.getNumberOfFixedWarnings()
-    println result.getNumberOfHighPriorityWarnings()
-    println result.getNumberOfNormalPriorityWarnings()
-    println result.getNumberOfLowPriorityWarnings()
-    println high + normal + low
+    // println result.getNumberOfNewWarnings()
+    // println result.getNumberOfFixedWarnings()
+    // println result.getNumberOfHighPriorityWarnings()
+    // println result.getNumberOfNormalPriorityWarnings()
+    // println result.getNumberOfLowPriorityWarnings()
+    // println high + normal + low
 
-    info = [
-        newWarnings: result.getNumberOfNewWarnings(),
-        fixedWarnings: result.getNumberOfFixedWarnings(),
-        high: result.getNumberOfHighPriorityWarnings(),
-        normal: result.getNumberOfNormalPriorityWarnings(),
-        low: result.getNumberOfLowPriorityWarnings(),
-        total: high + normal + low,
-        description: ''
-    ]
+    // info = [
+    //     newWarnings: result.getNumberOfNewWarnings(),
+    //     fixedWarnings: result.getNumberOfFixedWarnings(),
+    //     high: result.getNumberOfHighPriorityWarnings(),
+    //     normal: result.getNumberOfNormalPriorityWarnings(),
+    //     low: result.getNumberOfLowPriorityWarnings(),
+    //     total: high + normal + low,
+    //     description: ''
+    // ]
 
-    switch (info) {
-        case { it.newWarnings == 0 && it.fixedWarnings == 0 }:
-            info.description = "PyLint found no new or fixed issues."
-            break;
-        case info.fixedWarnings == 0:
-            info.description = "PyLint found ${info.newWarnings} new issues."
-            break;
-        case info.newWarnings == 0:
-            info.description = "PyLint found ${info.fixedWarnings} fixed issues."
-            break;
-        default:
-            info.description = "PyLint found ${info.newWarnings} new and ${info.fixedWarnings} fixed issues."
-            break;
-    }
+    // switch (info) {
+    //     case { it.newWarnings == 0 && it.fixedWarnings == 0 }:
+    //         info.description = "PyLint found no new or fixed issues."
+    //         break;
+    //     case info.fixedWarnings == 0:
+    //         info.description = "PyLint found ${info.newWarnings} new issues."
+    //         break;
+    //     case info.newWarnings == 0:
+    //         info.description = "PyLint found ${info.fixedWarnings} fixed issues."
+    //         break;
+    //     default:
+    //         info.description = "PyLint found ${info.newWarnings} new and ${info.fixedWarnings} fixed issues."
+    //         break;
+    // }
 
     return info
 }
